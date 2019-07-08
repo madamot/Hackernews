779a8716ae92962bc3d4197492260fa843a23910
Madamot <adam@madamot.com>
2019-07-08 11:13:12 +0100

    pulling from API sample data removed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walkie',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 
 function isSearched(searchTerm) {
   return function (item) {
@@ -32,12 +20,27 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -51,7 +54,12 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) {
+      return null;
+    }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -62,7 +70,7 @@ class App extends Component {
             Search
           </Search>
           <Table
-            list={list}
+            list={result.hits}
             pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
@@ -107,18 +115,14 @@ const Table = ({ list, pattern, onDismiss }) =>
     )}
   </div>
 
-  const Button = ({
-  	  onClick,
-  	  className = '',
-  	  children,
-  	}) =>
-  	  <button
-  	    onClick={onClick}
-  	    className={className}
-  	    type="button"
-     >
-  	    {children}
-  	  </button>
+  const Button = ({ onClick, className = '', children }) =>
+	  <button
+	    onClick={onClick}
+	    className={className}
+	    type="button"
+   >
+	    {children}
+	  </button>
 
 
 
